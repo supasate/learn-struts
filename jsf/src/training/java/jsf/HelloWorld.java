c9356c51e51d5ad4e4366112bea0b67335904c43
Supasate Choochaisri <supasate.c@gmail.com>
2014-11-29 21:44:15 -0600

    Add Hello World class and ignore build dir.

---
+++ jsf/src/training/java/jsf/HelloWorld.java
@@ -0,0 +1,30 @@
+package training.java.jsf;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean(name = "helloWorld", eager = true)
+@RequestScoped
+public class HelloWorld {
+
+	@ManagedProperty(value="#{message}")
+	private Message messageBean;
+
+	private String message;
+
+	public HelloWorld() {
+		System.out.println("Hello World started!");
+	}
+
+	public String getMessage() {
+		if (messageBean != null) {
+			message = messageBean.getMessage();
+		}
+		return message;
+	}
+
+	public void setMessageBean(Message message) {
+		this.messageBean = message;
+	}
+}
